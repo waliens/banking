271f5fc52c4bd264c5af191534667da705133eaa
waliens <r.mormont@uliege.be>
2021-12-07 19:18:11 +0100

    server init file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/style.scss';
 
-Vue.config.productionTip = false
+import { make_i18n_jsons } from './utils/export_i18n_csv';
+make_i18n_jsons("./locales/translations.csv", "./locales/json")
+
+import i18n from './i18n';
+
+
+import moment from 'moment';
+moment.locale(window.navigator.userLanguage || window.navigator.language || 'en');
+moment.tz.setDefault(moment.tz.guess());
+Vue.use(require('vue-moment'), {moment});
+
+import VueRouter from 'vue-router';
+import router from './router';
+Vue.use(VueRouter);
+
+import store from './store';
+
+import Buefy from 'buefy';
+Vue.use(Buefy, {defaultIconPack: 'fas'});
+
+import axios from 'axios';
+axios.defaults.baseURL = process.env.VUE_APP_API;
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  i18n,
+  router,
+  store
+}).$mount('#app');
