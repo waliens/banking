1ac88a5247f2dedc6488e9d34094fb99a59f0a90
waliens <r.mormont@uliege.be>
2022-01-03 00:34:07 +0100

    implement account group home page

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
       <h1 class="title">Group <em>{{group.name}}</em></h1>
+      <section class="level">
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">{{$t('account_group.number_accounts')}}</p>
+            <p class="title">{{group.accounts.length}}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">{{$t('balance')}}</p>
+            <p class="title">{{overallBalance}}</p>
+          </div>
+        </div>
+      </section>
       <section>
         <account-table :accounts="group.accounts"></account-table>
       </section>
@@ -10,6 +24,7 @@
 <script>
 import { defineComponent } from '@vue/composition-api'
 import AccountTable from '../components/accounts/AccountTable.vue';
+import currency from 'currency.js';
 
 export default defineComponent({
   components: {AccountTable},
@@ -19,7 +34,12 @@ export default defineComponent({
   computed: {
     group() {
       return this.$store.state.currentGroup;
-    } 
+    },
+    overallBalance() {
+      let balance = currency(0);
+      console.log(this.group);
+      return this.group.accounts.map(a => currency(a.balance)).reduce((o, b) => o.add(b), balance);
+    }
   }
 })
 </script>
