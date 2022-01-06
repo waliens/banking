ad30a0f10bf82bc0c6144e4fa5946c102a85e14c
waliens <r.mormont@uliege.be>
2022-01-06 16:03:48 +0100

    add buttons in navbar

--- frontend/src/components/layout/LayoutNavbar.vue
+++ frontend/src/components/layout/LayoutNavbar.vue
@@ -1,12 +1,17 @@
 <template>
   <b-navbar class="is-primary">
     <template #brand>
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+      <b-navbar-item tag="router-link" :to="{ name: 'home' }">
         {{$t("app_name")}}
       </b-navbar-item>
     </template>
+    <template #start>
+      <b-navbar-item tag="router-link" :to="{ name: 'merge-accounts' }">
+        {{$t('account.merge')}}
+      </b-navbar-item>
+    </template>
     <template #end>
-        <b-navbar-item tag="div">
+        <b-navbar-item tag="router-link" :to="{ name: 'select-account-group' }">
           <div class="group info">
             <b-tag :type="tag_class">Group: <em>{{!!group ? group.name : $t('account_group.not_selected')}}</em></b-tag>
           </div>
