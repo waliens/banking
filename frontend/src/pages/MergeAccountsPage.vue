c8aef4199aa7912fa31c7410470043ec629665c3
waliens <r.mormont@uliege.be>
2022-01-06 22:59:17 +0100

    implement levenshtein auto matcher

--- frontend/src/pages/MergeAccountsPage.vue
+++ frontend/src/pages/MergeAccountsPage.vue
@@ -6,6 +6,29 @@
         <div class="level-left"> <b-button v-on:click="mergeWarning" class="level-item is-small" icon-right="cog">{{$t('merge')}}</b-button></div>
       </div>
     </section>
+    <section class="level">
+      <div class="level-left">
+        <p class="level-item">{{$t('string.matching.auto-match')}}</p>
+        <b-field class="match-field-level level-item" :label="$t('string.matching.strategy')" label-position="on-border">
+          <b-select placeholder="Select a name" v-model="matchStrategy" size="is-small" :disabled="!selectedRepr" expanded>
+            <option
+                v-for="strategy in matchStrategies"
+                :value="strategy"
+                :key="strategy">
+                {{strategy}}
+            </option>
+          </b-select>
+        </b-field>
+        <b-field class="match-field-level level-item">
+          <b-button icon-right="chevron-left" size="is-small" :disabled="!selectedRepr || matchingCurrentIndex <= 0" @click="prevMatch"></b-button>
+        </b-field>
+        <b-field class="match-field-level level-item">
+          <b-button icon-right="chevron-right" size="is-small" :disabled="!selectedRepr || matchingCurrentIndex >= matchCandidates.length - 1" @click="nextMatch"></b-button>
+        </b-field>
+        <p>{{$t('string.matching.matches')}}: <em>{{matchingCurrentIndex+1}} / {{matchCandidates.length}}</em></p>
+      </div>
+
+    </section>
     <section>
       <div class="columns">
         <div class="column">
@@ -43,6 +66,7 @@ import { defineComponent } from '@vue/composition-api'
 import AccountDropDownSelector from '../components/accounts/AccountDropDownSelector.vue'
 import Account from '@/utils/api/Account';
 import AccountAliasTable from '../components/accounts/AccountAliasTable.vue';
+import { getStrategies, matchAndSortArray } from '@/utils/stringmatch';
 
 export default defineComponent({
   components: { AccountDropDownSelector, AccountAliasTable },
@@ -50,7 +74,11 @@ export default defineComponent({
     return {
       accounts: [],
       selectedRepr: null,
-      selectedAlias: null
+      selectedAlias: null,
+      matchStrategies: getStrategies(),
+      matchStrategy: getStrategies()[0],
+      matchingCurrentIndex: -1,
+      matchCandidates: []
     };
   },
   async created() {
@@ -81,7 +109,7 @@ export default defineComponent({
         type: 'is-warning',
         hasIcon: true,
         onConfirm: this.merge
-      })
+      });
     },
     async merge() {
       if (!this.selectedRepr || !this.selectedAlias) {
@@ -97,11 +125,58 @@ export default defineComponent({
       let tmp = this.selectedRepr;
       this.selectedRepr = this.selectedAlias;
       this.selectedAlias = tmp;
+    },
+    getMatchCandidates() {
+      console.log("eval");
+      console.log(this.selectedRepr);
+      if (!this.selectedRepr || !this.selectedRepr.name) {
+        return [];
+      }
+      console.log("not sorted");
+      let sorted = matchAndSortArray(
+        this.matchStrategy, 
+        this.selectedRepr,
+        this.accounts.filter(account => account.id != this.selectedRepr.id && account.name), 
+        o => o.name  
+      );
+      console.log("sorted");
+      return sorted;
+    },
+    nextMatch() {
+      this.matchingCurrentIndex++;
+      this.selectedAlias = this.matchCandidates[this.matchingCurrentIndex].obj;
+    },
+    prevMatch() {
+      this.matchingCurrentIndex--;
+      this.selectedAlias = this.matchCandidates[this.matchingCurrentIndex].obj;
+    }
+  },
+  computed: {
+   
+  },
+  watch: {
+    matchStrategy: function(newStrategy, oldStrategy) {
+      if (newStrategy != oldStrategy) {
+        this.matchingCurrentIndex = -1;
+        this.selectedAlias = null;
+        this.matchCandidates = this.getMatchCandidates();
+      }
+    },
+    selectedRepr: function(newRepr, oldRepr) {
+      if (newRepr != oldRepr || (newRepr && oldRepr && newRepr.id != oldRepr.id)) {
+        this.matchingCurrentIndex = -1;
+        if (newRepr) {
+          this.matchCandidates = this.getMatchCandidates();
+        }
+      } 
     }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-
+.match-field-level {
+  margin-bottom: 5px;
+  margin-left: 5px;
+}
 </style>
---
+++ frontend/src/utils/stringmatch.js
@@ -0,0 +1,25 @@
+import levenshtein from "js-levenshtein";
+
+export let LEVENSHTEIN = 'levenshtein';
+
+export function getStrategies() {
+  return [
+    LEVENSHTEIN,
+  ];
+}
+
+export function matchScore(strategy, reference, other, keyFn) {
+  let strRef = keyFn(reference), strOther = keyFn(other);
+  if (strategy == LEVENSHTEIN) {
+    return levenshtein(strRef, strOther);
+  } else {
+    throw new Error("unknown string match scoring strategy '" + strategy + "'");
+  }
+}
+
+export function matchAndSortArray(strategy, reference, array, keyFn) {
+  let withScore = array.map(obj => { return {obj, score: matchScore(strategy, reference, obj, keyFn) }; });
+  withScore.sort((a, b) => a.score - b.score);
+  console.log(withScore);
+  return withScore;
+}
