591a378e3d4b639d0e4a872f551db1ce3699622d
waliens <r.mormont@uliege.be>
2022-02-01 02:02:11 +0100

    remove console.log's

--- frontend/src/utils/api/Account.js
+++ frontend/src/utils/api/Account.js
@@ -29,26 +29,19 @@ export default class Account extends Model {
   }
 
   async updateChange(data) {
-    console.log("whaaat");
     let payload = {};
     let doUpdate = false;
-    console.log(payload);
-    console.log(hasOwnProperty(data, 'initial'));
-    console.log(currency(this.initial).intvalue != currency(data.initial).intvalue);
     if(hasOwnProperty(data, 'initial') && currency(this.initial).intValue != currency(data.initial).intValue) {
-      console.log("update initial");
       payload.initial = data.initial;
       doUpdate = true;
     }
     if(hasOwnProperty(data, 'representative') && data.representative.id > 0) {
-      console.log("update repre");
       payload.id_representative = data.representative.id;
       doUpdate = true;
     }
     if (!doUpdate) {
       return this;
     }
-    console.log("before requestsd");
     let result = await axios.put(this.uri, payload);
     return result.data;
   }
