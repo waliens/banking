4f90e078bdd391f342eaf6a509ada948fd093f4f
waliens <r.mormont@uliege.be>
2022-02-07 16:55:16 +0100

    parent dropdown

--- frontend/src/pages/EditTagTreePage.vue
+++ frontend/src/pages/EditTagTreePage.vue
@@ -9,13 +9,18 @@
     </section>
     <section class="column">
       <section class="level title-section">
-        <div class="level-left"><h3 class="level-item title">{{$t('category')}}</h3></div>
+        <div class="level-left">
+          <h3 class="level-item title">{{$t('category')}}</h3>
+        </div>
         <div class="level-right">
           <b-button v-if="selected.id" class="level-item is-small is-danger" @click="() => {}" icon-right="times">{{$t('delete')}}</b-button>
           <b-button :disabled="!isFormValid()" class="level-item is-small is-primary" @click="() => {}" icon-right="pen">{{$t('save')}}</b-button>
         </div>
       </section>
       <section>
+        <div class="breadcrumb">
+          <category-chain-breadcrumb :category-tree="tree" :parent-category-id="selected.id_parent"></category-chain-breadcrumb>
+        </div>
         <b-field :label="$t('tagging.tag.name')" label-position="on-border">
           <b-input v-model="selected.name"></b-input>
         </b-field>
@@ -25,14 +30,18 @@
         <b-field :label="$t('tagging.tag.icon')" label-position="on-border">
           <b-input v-model="selected.icon"></b-input>
         </b-field>
+        <b-field :label="$t('tagging.tag.parent')" label-position="on-border" >
+          <b-select v-model="selected.id_parent" expanded>
+            <option v-for="chain in allStringBreadcrumbs" :key="chain.id" :value="chain.id">
+              {{chain.breadcrumb}}
+            </option>
+          </b-select>
+        </b-field>
         <b-field>
           <b-checkbox v-model="selected.income">{{$t('tagging.tag.default')}}</b-checkbox>
         </b-field>
         <b-field>
           <b-checkbox v-model="selected.income">{{$t('tagging.tag.income')}}</b-checkbox>
-        </b-field>
-        <b-field :label="$t('tagging.tag.parents')" horizontal position="is-left">
-          <category-chain-breadcrumb :category-tree="tree" :parent-category-id="selected.id_parent"></category-chain-breadcrumb>
         </b-field>
       </section>
     </section>
@@ -57,7 +66,33 @@ export default defineComponent({
   async created() {
     this.tree = await Category.getCategoryTree();
   },
+  computed: {
+    allStringBreadcrumbs() {
+      if (this.tree.length == 0) {
+        return [];
+      }
+      return this.recurGetAllStringBreadcrumbs(this.tree);
+    }
+  },
   methods: {
+    recurGetAllStringBreadcrumbs(nodes) {
+      if (nodes.length == 0) {
+        return [];
+      }
+      let currentBreadcrumbs = [];
+      for (let nodeIdx in nodes) {
+        let node = nodes[nodeIdx];
+        let childrenBreadcrumbs = this.recurGetAllStringBreadcrumbs(node.children);
+
+        for (let childBrcmbIdx in childrenBreadcrumbs) {
+          let childBrcmb = childrenBreadcrumbs[childBrcmbIdx];
+          childBrcmb.breadcrumb = [node.name, childBrcmb.breadcrumb].join(" > ")
+          currentBreadcrumbs.push(childBrcmb);
+        }
+        currentBreadcrumbs.push({id: node.id, breadcrumb: node.name })
+      }
+      return currentBreadcrumbs;
+    },
     isFormValid() {
       return false;
     }
@@ -70,5 +105,10 @@ export default defineComponent({
 .scrollable {
   max-height: 100vh;
   overflow-y: auto;
+}
+
+.breadcrumb {
+  margin-top: 0;
+  margin-bottom: 30px;
 }
 </style>
