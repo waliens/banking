d9b7eb712f1336143bb67877359252c14424326d
Romain Mormont <romain.mormont@cytomine.com>
2023-12-16 12:44:23 +0100

    build: build and push script

---
+++ scripts/build-push-all.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    echo "no version provided, skipping build"
+    exit 1
+else
+    echo "building for version $VERSION"
+fi
+
+echo "building server..."
+docker build -t rmormont/banking-server:latest -t rmormont/banking-server:$VERSION --target server server/
+if [ $? -ne 0 ]; then
+  echo "/!\ server build failed, aborting..."
+fi
+
+echo "building task runner..."
+docker build -t rmormont/banking-task-runner:latest -t rmormont/banking-task-runner:$VERSION --target server server/
+if [ $? -ne 0 ]; then
+  echo "/!\ task-runner build failed, aborting..."
+fi
+
+echo "building frontend..."
+docker build -t rmormont/banking-ui:latest -t rmormont/banking-ui:$VERSION --target prod-server frontend/
+if [ $? -ne 0 ]; then
+  echo "/!\ ui build failed, aborting..."
+fi
+
+# echo "pusing all images"
+docker push rmormont/banking-server:latest
+docker push rmormont/banking-server:$VERSION
+docker push rmormont/banking-task-runner:latest
+docker push rmormont/banking-task-runner:$VERSION 
+docker push rmormont/banking-ui:latest
+docker push rmormont/banking-ui:$VERSION 
