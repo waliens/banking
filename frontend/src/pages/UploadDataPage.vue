c82bee84460dcb6b12d7895e24ae55159dea2eb1
Romain Mormont <romain.mormont@cytomine.com>
2023-12-27 17:16:53 +0100

    fix: upload data page

--- frontend/src/pages/UploadDataPage.vue
+++ frontend/src/pages/UploadDataPage.vue
@@ -109,7 +109,7 @@ export default defineComponent({
         args.id_mscard_account = this.mastercardAccount.id;
       }
       await Model.uploadFiles(this.files, "/upload_files", args).then(() => {
-        this.$router.push({name: 'dashboard'});
+        return this.$router.push({name: 'dashboard'}).catch(() => {});
       }).catch(err => {
         this.$buefy.dialog.alert({
           title: 'Error',
