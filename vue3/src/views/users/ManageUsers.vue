ea9c4c77d477e217886fae2fbfee79bd734322c3
Romain Mormont <romain.mormont@cytomine.com>
2024-08-26 20:46:44 +0200

    fix: change style

--- vue3/src/views/users/ManageUsers.vue
+++ vue3/src/views/users/ManageUsers.vue
@@ -32,8 +32,8 @@ const menuItems = computed(() => {
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-x-4">
-    <section class="col-span-1">
+  <div class="flex flex-rows flex-auto">
+    <section>
       <Menu class="scrollable p-2" :model="menuItems" >
         <template #start>
           <div class="container flex flew-row justify-center">
@@ -42,28 +42,8 @@ const menuItems = computed(() => {
         </template>
       </Menu>
     </section>
-    <section class="w-full col-span-3">
+    <section class="ml-4 w-full">
       <RouterView />
     </section>
-    <!-- <section class="col-span-3">
-      <section class="flex items-center justify-between">
-        <h3 class="text-xl">
-          <span v-if="activeUser && activeUser.id">{{ t('user.edit') }}</span>
-          <span v-else>{{ t('user.create') }}</span>
-        </h3>
-        <Button :disabled="!isFormValid()" class="is-small is-primary" @click="saveUser" icon="pi pi-save">{{t('save')}}</Button>
-      </section>
-      <section v-if="isAnyUserMenuActive">
-        <Field :label="t('user.username')" label-position="on-border" :class="{ 'is-danger': activeUser.username.length === 0 }">
-          <Input v-model="activeUser.username"></Input>
-        </Field>
-        <Field :label="t('user.password')" label-position="on-border" :class="{ 'is-danger': !passwordDoMatch }">
-          <Input type="password" v-model="activeUser.password"></Input>
-        </Field>
-        <Field :label="t('user.repeat_password')" label-position="on-border" :class="{ 'is-danger': !passwordDoMatch }">
-          <Input type="password" v-model="activeUser.password2"></Input>
-        </Field>
-      </section>
-    </section> -->
   </div>
 </template>
