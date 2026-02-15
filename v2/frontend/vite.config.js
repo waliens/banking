20ab2fe928cba5cb6f0cffa5a622fb8a2ee51d56
Romain Mormont <romainmormont@hotmail.com>
2026-02-15 13:26:40 +0100

    feat: use env var for backend url

--- v2/frontend/vite.config.js
+++ v2/frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api/v2': {
-        target: 'http://localhost:8000',
+        target: process.env.API_URL || 'http://localhost:8000',
         changeOrigin: true
       }
     }
